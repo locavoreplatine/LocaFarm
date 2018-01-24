0351de53caec3e4e90478659112cb822a3464e2a
Aoudia <mf.aoudia@gmail.com>
2018-01-24 20:19:27 +0100

    ci : Add Google Maps key injection to Android Manifest

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -2,8 +2,16 @@
 
 function copyEnvVarsToLocalProperties {
     LOCAL_PROPERTIES=$HOME"/locafarm/local.properties"
+    ANDROID_MANIFEST=$HOME"/locafarm/app/src/main/AndroidManifest.xml"
+
     export LOCAL_PROPERTIES
     echo "Local Properties should exist at $LOCAL_PROPERTIES"
+
+    export ANDROID_MANIFEST
+    echo "AndroidManifest should exist at $ANDROID_MANIFEST"
+
+    sed -i -e "s/\"\${google_map_api_key}\"/"\"$GOOGLE_MAP_API_KEY\""/g" $ANDROID_MANIFEST
+
 
     if [ ! -f "$LOCAL_PROPERTIES" ]; then
         echo "Gradle local does not exist"
@@ -15,4 +23,5 @@ function copyEnvVarsToLocalProperties {
         echo "googlemapapikey=$GOOGLE_MAP_API_KEY" > $LOCAL_PROPERTIES
 
     fi
+
 }
